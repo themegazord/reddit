851d02faa833b5a8a1ce23a0a478dd57ef1c735a
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-07-04 23:02:18 -0400

    project initalized

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,10 @@
+<div class="flex flex-col">
+  {{-- region navbar --}}
+  <div class="flex flex-row justify-between items-center px-2">
+    <div class="flex flex-row gap-1 h-6">
+      <livewire:svg.logo />
+      <livewire:svg.nome />
+    </div>
+  </div>
+  {{-- endregion navbar --}}
+</div>
